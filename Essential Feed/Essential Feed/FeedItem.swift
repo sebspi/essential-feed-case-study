ad86cbf4e4a36a2a0fd18560a7d44d9ab97fbc2b
Sebastian Spies <sebastian@appsoluts.de>
2022-02-19 14:34:51 +0100

    Create the FeedItem model

---
+++ Essential Feed/Essential Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Essential Feed
+//
+//  Created by Sebastian Spies on 19.02.22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let desription: String?
+    let location: String?
+    let imageURL: URL
+}
