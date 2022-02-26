d701ede03954cc296ac5159b615a0d65e870e68f
Sebastian Spies <sebspi91@gmail.com>
2022-02-26 15:18:56 +0100

    Move URL creation to a shared factory method

--- Essential Feed/Essential FeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ Essential Feed/Essential FeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -37,7 +37,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -52,7 +52,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError() {
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL()
         let error = NSError(domain: "the error", code: 0)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
@@ -80,6 +80,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return URLSessionHTTPClient()
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
     }
     
     private class URLProtocolStub: URLProtocol {
