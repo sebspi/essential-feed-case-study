4f35d813e91f4e6c2f080a4f42046b8e289ab1b7
Sebastian Spies <sebspi91@gmail.com>
2022-03-08 19:33:04 +0100

    Added `.ephemeral` url session to prevent caching in end-to-end tests

--- Essential Feed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ Essential Feed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -33,7 +33,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: .init(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(client, file: file, line: line)
