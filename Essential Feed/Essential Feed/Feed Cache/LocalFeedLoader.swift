ffb75652eec90d2cc3f904a51cfb3b56b3798d5f
Sebastian Spies <sebspi91@gmail.com>
2022-03-12 13:47:36 +0100

    Load command delivers no images on seven days old cache

--- Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
+++ Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
@@ -11,6 +11,7 @@ public final class LocalFeedLoader {
     
     private let store: FeedStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
 
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
@@ -32,16 +33,25 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { result in
+        store.retrieve { [unowned self] result in
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-            case .empty:
-                completion(.success([]))
-            case let .found(feed, _):
+            case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
+            case .found, .empty:
+                completion(.success([]))
             }
         }
+    }
+    
+    private let maxAgeDays = 7
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxAgeDays, to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
     }
     
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
--- Essential Feed/Essential FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ Essential Feed/Essential FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -50,6 +50,16 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoImagesSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT()
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: sevenDaysOldTimestamp)
+        })
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
