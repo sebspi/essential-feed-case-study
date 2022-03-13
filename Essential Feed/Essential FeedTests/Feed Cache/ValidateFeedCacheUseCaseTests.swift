7b6479f33557dc7545890f902950f609a732ff1c
Sebastian Spies <sebspi91@gmail.com>
2022-03-13 21:04:27 +0100

    LocalFeedLoader does not message store upon creation (before validating the cached feed)

---
+++ Essential Feed/Essential FeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,27 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  Essential FeedTests
+//
+//  Created by Sebastian Spies on 13.03.22.
+//
+
+import XCTest
+import Essential_Feed
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageTheStoreUponCreation() {
+        let (_, store) = makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
