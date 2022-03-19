6e2bf6ac61f545ec3e6fefe3db8538c23e4ca20f
Sebastian Spies <sebspi91@gmail.com>
2022-03-19 15:50:30 +0100

    Added `CoreDataFeedStore`

--- Essential Feed/Essential FeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ Essential Feed/Essential FeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -1,61 +1,92 @@
 //
-//  CoreDataFeedStoreTests.swift
-//  Essential FeedTests
-//
-//  Created by Sebastian Spies on 19.03.22.
+//  Copyright © 2019 Essential Developer. All rights reserved.
 //
 
-import Foundation
 import XCTest
+import Essential_Feed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache() {
-        
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        
-    }
-    
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        
-    }
-    
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-        
-    }
-    
-    func test_insert_deliversNoErrorOnEmptyCache() {
-        
-    }
-    
-    func test_insert_deliversNoErrorOnNonEmptyCache() {
-        
-    }
-    
-    func test_insert_overridesPreviouslyInsertedCacheValues() {
-        
-    }
-    
-    func test_delete_deliversNoErrorOnEmptyCache() {
-        
-    }
-    
-    func test_delete_hasNoSideEffectsOnEmptyCache() {
-        
-    }
-    
-    func test_delete_deliversNoErrorOnNonEmptyCache() {
-        
-    }
-    
-    func test_delete_emptiesPreviouslyInsertedCache() {
-        
-    }
-    
-    func test_storeSideEffects_runSerially() {
-        
-    }
-    
-    
+	
+	func test_retrieve_deliversEmptyOnEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+	}
+	
+	func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+	}
+	
+	func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+	}
+	
+	func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
+	}
+	
+	func test_insert_deliversNoErrorOnEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+	}
+	
+	func test_insert_deliversNoErrorOnNonEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+	}
+	
+	func test_insert_overridesPreviouslyInsertedCacheValues() {
+		let sut = makeSUT()
+		
+		assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+	}
+	
+	func test_delete_deliversNoErrorOnEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+	}
+	
+	func test_delete_hasNoSideEffectsOnEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+	}
+	
+	func test_delete_deliversNoErrorOnNonEmptyCache() {
+		let sut = makeSUT()
+		
+		assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+	}
+	
+	func test_delete_emptiesPreviouslyInsertedCache() {
+		let sut = makeSUT()
+		
+		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+	}
+	
+	func test_storeSideEffects_runSerially() {
+		let sut = makeSUT()
+		
+		assertThatSideEffectsRunSerially(on: sut)
+	}
+
+	// - MARK: Helpers
+	
+	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+		let storeBundle = Bundle(for: CoreDataFeedStore.self)
+		let storeURL = URL(fileURLWithPath: "/dev/null")
+		let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return sut
+	}
+
 }
