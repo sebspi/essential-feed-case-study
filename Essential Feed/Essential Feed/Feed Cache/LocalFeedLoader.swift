37c52fcb401d1a41165e8f440b1306fb3fd63bc8
Sebastian Spies <sebspi91@gmail.com>
2022-03-27 12:16:32 +0200

    Replace custom `RetrievalCachedFeedResult` enum with a nested type alias over the standard `Swift.Result` (`FeedStore.RetrievalResult`)

--- Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
+++ Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
@@ -40,10 +40,10 @@ public final class LocalFeedLoader: FeedLoader {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .found(feed, timestamp) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .success(.found(feed, timestamp)) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(feed.toModels()))
                 
-            case .found, .empty:
+            case .success:
                 completion(.success([]))
             }
         }
@@ -64,7 +64,7 @@ public final class LocalFeedLoader: FeedLoader {
             switch result {
             case .failure:
                 self.store.deleteCachedFeed(completion: { _ in})
-            case let .found(_, timestamp) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .success(.found(_, timestamp)) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedFeed(completion: { _ in })
             default: break
             }
