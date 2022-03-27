0f06c05e4340f40c4472406a9c8855953f72c725
Sebastian Spies <sebspi91@gmail.com>
2022-03-27 12:29:12 +0200

    Refactor `CachedFeed` type from `enum` to `tuple` since we can represent the absence of a values with an `optional`

--- Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
+++ Essential Feed/Essential Feed/Feed Cache/LocalFeedLoader.swift
@@ -40,8 +40,8 @@ public final class LocalFeedLoader: FeedLoader {
             case let .failure(error):
                 completion(.failure(error))
                 
-            case let .success(.found(feed, timestamp)) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
-                completion(.success(feed.toModels()))
+            case let .success(.some(cache)) where FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
+                completion(.success(cache.feed.toModels()))
                 
             case .success:
                 completion(.success([]))
@@ -64,7 +64,7 @@ public final class LocalFeedLoader: FeedLoader {
             switch result {
             case .failure:
                 self.store.deleteCachedFeed(completion: { _ in})
-            case let .success(.found(_, timestamp)) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .success(.some(cache)) where !FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
                 self.store.deleteCachedFeed(completion: { _ in })
             default: break
             }
